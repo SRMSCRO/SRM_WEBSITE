e19a9615aa2911615887f9fb9b584785218f4d6a
Tanisha Bisht <agamyatani@gmail.com>
2021-03-08 11:05:07 +0530

    map added, quick-info resposive

--- Assets/script.js
+++ Assets/script.js
@@ -7,6 +7,10 @@
     var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
     window.kommunicate = m; m._globals = kommunicateSettings;
 })(document, window.kommunicate || {});
+
+
+
+
 // OWL CAROUSEL
 $(document).ready(function() {
     $("#research-slider").owlCarousel({
@@ -113,3 +117,11 @@ $(function () {
 
 
 
+// MAP
+function myMap() {
+    var mapProp= {
+        center:new google.maps.LatLng(51.508742,-0.120850),
+        zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
