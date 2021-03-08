c7ac0878a16e12199d5e47a35c779447ea3f66c7
Siddharth451 <54898490+Siddharth451@users.noreply.github.com>
2021-03-08 08:26:55 +0530

    position of chatbot changed

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,14 @@
+
+// CHATBOT
+(function(d, m){
+    var kommunicateSettings =
+        {"appId":"3cd244ecc29389ece6bd830aeb6ae6df5","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+})(document, window.kommunicate || {});
+
 // OWL CAROUSEL
 $(document).ready(function() {
     $("#research-slider").owlCarousel({
@@ -102,14 +113,3 @@ $(function () {
 })
 
 
-
-
-// CHATBOT
-(function(d, m){
-    var kommunicateSettings =
-        {"appId":"3cd244ecc29389ece6bd830aeb6ae6df5","popupWidget":true,"automaticChatOpenOnNavigation":true};
-    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-    window.kommunicate = m; m._globals = kommunicateSettings;
-})(document, window.kommunicate || {});
